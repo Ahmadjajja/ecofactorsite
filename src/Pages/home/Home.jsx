2ee588b8dabc7ed06390d6965fc9ba208d3d87bd
Ahmadjajja <ahmadjajja86@gmail.com>
2024-04-23 22:38:01 +0500

    updated

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -4,7 +4,6 @@ import EcofactorAccess from "../../Components/home sections/EcofactorAccess";
 import HeroSection from "../../Components/home sections/HeroSection";
 import MainFeatures from "../../Components/home sections/MainFeatures";
 import Team from "../../Components/home sections/Team";
-import Testimonial from "../../Components/home sections/Testimonial";
 import WhoWeAre from "../../Components/home sections/WhoWeAre";
 import WhyEcofactor from "../../Components/home sections/WhyEcofactor";
 
